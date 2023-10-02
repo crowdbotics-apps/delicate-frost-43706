1f9b5c5e36cf587f103ba26ca07347d379eab430
Crowdbotics <team@crowdbotics.com>
2023-10-02 11:32:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-frost-43706",
+  url: "https://delicate-frost-43706.botics.co",
+  api: "https://delicate-frost-43706.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
